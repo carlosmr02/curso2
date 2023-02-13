c533492ff8e817f451c64706a24fff59e5ddbe1f
Carlos <alumno.254370@ies-azarquiel.es>
2023-02-13 23:49:34 +0100

    Commit UsuarioDaoImp 2:06

---
+++ curso/src/main/java/com/cursojava/curso/dao/UsuarioDaoImp.java
@@ -0,0 +1,18 @@
+package com.cursojava.curso.dao;
+
+import com.cursojava.curso.models.Usuario;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+@Transactional
+
+public class UsuarioDaoImp implements UsuarioDao {
+
+    @Override
+    public List<Usuario> getUsuarios() {
+        return null;
+    }
+}
