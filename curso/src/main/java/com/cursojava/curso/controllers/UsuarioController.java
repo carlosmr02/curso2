afd6184a264e1a54e2658491fe1fd61be0738d4e
Carlos <alumno.254370@ies-azarquiel.es>
2023-03-05 20:41:39 +0100

    Commit implementación de argon2 3:40

--- curso/src/main/java/com/cursojava/curso/controllers/UsuarioController.java
+++ curso/src/main/java/com/cursojava/curso/controllers/UsuarioController.java
@@ -2,6 +2,8 @@ package com.cursojava.curso.controllers;
 
 import com.cursojava.curso.dao.UsuarioDao;
 import com.cursojava.curso.models.Usuario;
+import de.mkammerer.argon2.Argon2;
+import de.mkammerer.argon2.Argon2Factory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -31,6 +33,9 @@ public class UsuarioController {
 
     @RequestMapping(value="api/usuarios",method= RequestMethod.POST)
     public void registraUsuario(@RequestBody Usuario usuario){
+        Argon2 argon2 = Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2id);
+        String hash = argon2.hash(1,1024,1, usuario.getPassword().getBytes());
+        usuario.setPassword(hash);
         usuarioDao.registraUsuario(usuario);
     }
 
